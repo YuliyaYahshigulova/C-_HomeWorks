f17016bc685e1be2457f1aa986930663d25bab5e
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-03 14:51:54 +0700

    add Task004 and chages of Descriptions

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+     Console.WriteLine(i);   
+    }
+    i++;
+}
