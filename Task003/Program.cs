251e6dbed8e252a33327f3740ebe176ba8a23d2c
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-03 12:03:18 +0700

    add Task003 and changes of Descriptions

---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Enter the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+   Console.WriteLine("Odd number"); 
+}
